ffc1c74b263549ad50f4be2ee46ab1479bde9ce0
rsmith013 <richard.d.smith@stfc.ac.uk>
2021-06-09 15:46:30 +0100

    added categorisation based on regex patterns. closes #8

--- asset_extractor/core/asset_extractor.py
+++ asset_extractor/core/asset_extractor.py
@@ -11,8 +11,10 @@ __contact__ = 'richard.d.smith@stfc.ac.uk'
 from asset_scanner.core import BaseExtractor
 from asset_scanner.core.handler_picker import HandlerPicker
 
+import re
 from typing import Optional, List
 import logging
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -33,9 +35,36 @@ class AssetExtractor(BaseExtractor):
     """
     PROCESSOR_ENTRY_POINT = 'asset_extractor.media_handlers'
 
+    def get_category(self, string, label, regex):
+        m = re.search(regex, string)
+
+        if not m:
+            label = 'data'
+
+        return label
+    
+    def get_categories(self, filepath, source_media, category_conf) -> dict:
+
+        categories = []
+
+        for conf in category_conf:
+            label = self.get_category(filepath, **conf)
+            if label:
+                categories.append(label)
+
+        return categories
+
     def process_file(self, filepath: str, source_media: str, checksum: Optional[str] = None) -> None:
+
+        # Get dataset description file
+        description = self.item_descriptions.get_description(filepath)
+
         processor = self.processors.get_processor(source_media)
 
         data = processor.run(filepath, source_media, checksum)
+        
+        categories = self.get_categories(filepath, source_media, description.categories)
+
+        data['body']['categories'] = categories
 
         self.output(data)
